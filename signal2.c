537eb286f64bbac8b533260a2a327ead2aa33672
Nosseir <nosseir6@gmail.com>
2024-10-29 13:03:42 +0100

    Modificado

--- signal2.c
+++ signal2.c
@@ -5,9 +5,10 @@
 
 void signal_handler(int numero)
 {
-	printf("Ha sonado la alarma");
-	alarm(1);
+	printf("Ha sonado la alarma\n");
 	signal(SIGALRM,signal_handler);
+	alarm(5);
+
 }
 
 
@@ -15,7 +16,7 @@ int main()
 {
 	
 	signal(SIGALRM,signal_handler);
-	alarm(1);	
+	alarm(5);	
 	while(1)
 	{
 		sleep(1);
